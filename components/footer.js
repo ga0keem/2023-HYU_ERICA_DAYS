97a5a0fb7b59843a319470ffda50021c43f89aa6
ga0keem <kky965912@gmail.com>
2023-02-24 21:53:55 +0900

    img 수정

--- components/footer.js
+++ components/footer.js
@@ -14,7 +14,7 @@ export default function Footer(){
                      </Link>
 
                     <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2023 DAYS — 
-                        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@ga0keem</a>
+                        <a href="https://www.instagram.com/ga0keem" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@ga0keem</a>
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         <a className="text-gray-500">
--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: [
-        'www.notion.so',
-        'images.unsplash.com',
-        's3.us-west-2.amazonaws.com'
-    ],
-    format: ['image/png', 'image/webp', 'image/jpeg']
+      domains: [
+          'www.notion.so',
+          'images.unsplash.com',
+          's3.us-west-2.amazonaws.com'
+      ],
+      format: ['image/png', 'image/webp', 'image/jpeg']
   }
 }
 
